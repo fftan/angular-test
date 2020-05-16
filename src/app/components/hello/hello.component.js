0373a4a490e311a776da67b67e219778b271e83b
tannvn@ames.edu.vn <tannvn@ames.edu.vn>
2020-05-16 15:46:21 +0700

    first commit

---
+++ src/app/components/hello/hello.component.js
@@ -0,0 +1,13 @@
+
+
+import { Component } from '@angular/core';
+
+Component({
+    selector: 'app-hello',
+    templete: `
+        <h2>Hello from hello component</h2>
+    `
+})
+export class HelloComponent {
+    
+}
